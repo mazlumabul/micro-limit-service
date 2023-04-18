2d8658808051b46a6f67c1cb4ab9bbac1a8c07b2
Mazlum ABUL <mazlumabul10@gmail.com>
2023-04-18 13:20:35 +0300

    initial

---
+++ src/test/java/com/mazlumabul/microservices/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazlumabul.microservices.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
